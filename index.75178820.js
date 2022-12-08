d78f6c496cb7864b7ee210ab9a3f6b3cab8b7dba
danypryzrak <116654935+danypryzrak@users.noreply.github.com>
2022-12-08 15:00:13 +0000

    Deploying to gh-pages from @ danypryzrak/team-pelmeni@cc31cf1124764e1bc1a85cf1eb3eb5eb13cf04a9 🚀

---
+++ index.75178820.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}document.querySelectorAll("[data-modal-open]").forEach((function(e){console.log("1"),e.addEventListener("click",o)})),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.75178820.js.map
