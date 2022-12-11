2f3f673d1294014f807479dbc3b959ad0ff51fe3
Vladyslav Mishchuk <v.mishuk2017@gmail.com>
2022-12-11 04:30:05 +0000

    finish project

--- src/js/slick-hero.js
+++ src/js/slick-hero.js
@@ -1,19 +1,45 @@
 let offset = 0;
 const sliderLine = document.querySelector('.hero__card');
 
-document.querySelector('.hero__slider-next').addEventListener('click', function(){
+document.querySelector('.hero__slider-prev').addEventListener('click', function(){
     offset = offset + 528;
     if (offset > 1056) {
         offset = 0;
     }
     sliderLine.style.left = -offset + 'px';
+
+
+    function foo() {
+        if (window.innerWidth > 1280) { 
+            offset = offset + 292;
+            if (offset > 876) {
+                offset = 0;
+            }
+            sliderLine.style.left = -offset + 'px';
+          }
+     }
+
 });
 
-document.querySelector('.hero__slider-prev').addEventListener('click', function () {
+document.querySelector('.hero__slider-next').addEventListener('click', function () {
     offset = offset - 528;
     if (offset < 0) {
         offset = 1056;
     }
     sliderLine.style.left = -offset + 'px';
+
+    function foo() {
+        if (window.innerWidth > 1280) { 
+            offset = offset - 292;
+            if (offset < 0) {
+                offset = 876;
+            }
+            sliderLine.style.left = -offset + 'px';
+          }
+     }
+
+
+
+
 });
 
