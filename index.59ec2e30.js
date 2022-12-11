ae58b0c494f9ceaa7f73334afff8a7a2700bdff0
danypryzrak <116654935+danypryzrak@users.noreply.github.com>
2022-12-11 07:30:19 +0000

    Deploying to gh-pages from @ danypryzrak/team-pelmeni@d296f57fd330f99a73eee5e9780426d964b661e6 🚀

--- index.59ec2e30.js
+++
@@ -1,2 +0,0 @@
-$(".one-time").slick({infinite:!0,speed:300,slidesToScroll:1,slidesToShow:1,adaptiveHeight:!0,responsive:[{breakpoint:768,settings:{slidesToShow:1}}]});
-//# sourceMappingURL=index.59ec2e30.js.map
